e961a51f1b2a9f2f69597117fa39e451daddc5f4
rfiet <r_fiet@yahoo.com>
2015-11-19 18:55:53 -0800

    Assignment 10

---
+++ part3/team2.js
@@ -0,0 +1,8 @@
+{
+  "item1": "I love jquery4u",
+  "item2": "You love jQuery4u",
+  "name": "Jennifer Lawrence",
+  "title": "Actress",
+  "item3": "We love jQuery4u",
+  "bio": "blah blah blah"
+}
